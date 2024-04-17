5aecdeae5fc08d37dd5fa4f285a6379d536d06cf
Ahmed Hlel <kirigaya.kazuto.hlel@gmail.com>
2024-04-17 18:24:33 +0100

    update authUtils file

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -1,6 +1,16 @@
 export const isAuthenticated = (): boolean => {
   const expiresAt = localStorage.getItem('expires_at');
-  return (
-    expiresAt !== null && parseFloat(expiresAt) > new Date().getTime() / 1000
-  );
+  console.log('expiresAt', expiresAt);
+
+  if (expiresAt) {
+    const expiresAtTimestamp = new Date(expiresAt).getTime() / 1000;
+    console.log('expiresAtTimestamp', expiresAtTimestamp);
+
+    const currentDate = new Date().getTime() / 1000;
+    console.log('currentDate', currentDate);
+
+    return expiresAtTimestamp > currentDate;
+  }
+
+  return false;
 };
